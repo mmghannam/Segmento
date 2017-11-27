14873af1f473018b7dfe2f390eb7d72d11aaa2a7
Mohamed Gamal <jimadness12@gmail.com>
2017-11-28 00:57:46 +0200

    Fixed N-Cut

--- ncut.py
+++ ncut.py
@@ -1,6 +1,7 @@
 import numpy as np
 import math
-from kmeans import KMeans
+from random import randint
+from sklearn.cluster import KMeans
 from data_reader import TRAIN_PATH
 from helpers import resize_image, euclidean_distance, show_image_from_data
 from PIL import Image
@@ -27,7 +28,7 @@ class NCut:
         elif self.algorithm == NCut.KNN:
             self.sim_matrix = self.__make_knn_sim_matrix(self.data, self.n_neighbors)
         self.n_cut_vector = self.__make_normalized_cut(self.sim_matrix, self.n_clusters)
-        self.new_image_data = self.__k_means_on_cut(self.n_cut_vector, self.n_clusters)
+        self.new_image_data = self.__k_means_on_cut(self.data, self.n_cut_vector, self.n_clusters)
         return self.new_image_data
 
     @staticmethod
@@ -53,23 +54,29 @@ class NCut:
         degree_matrix = np.zeros(sim_matrix.shape)
         for i in range(degree_matrix.shape[0]):
             degree_matrix[i, i] = sum(sim_matrix[i]) - 1
-        eig_values, eig_vectors = np.linalg.eigh(1 - np.linalg.pinv(degree_matrix).dot(sim_matrix))
-        idx = eig_values.argsort()
-        eig_vectors = eig_vectors[:, idx]
+        eig_vectors = np.linalg.eigh(degree_matrix - sim_matrix)[1]
         return normalize(eig_vectors[:, :n_clusters], axis=1, norm='l1')
 
     @staticmethod
-    def __k_means_on_cut(eig_vectors, clusters):
-        final_clustering = KMeans(eig_vectors, k=clusters, tol=1)
-        return final_clustering.assign()[1]
+    def __k_means_on_cut(data, eig_vectors, clusters):
+        new_data = []
+        cluster_colors = []
+        final_clustering = KMeans(clusters, n_jobs=-1)
+        assignments = final_clustering.fit_predict(eig_vectors)
+        for _ in range(clusters):
+            cluster_colors.append((randint(0, 255), randint(0, 255), randint(0, 255)))
+        for i in range(len(data)):
+            new_data.append(cluster_colors[assignments[i]])
+        return new_data
 
 
 if __name__ == "__main__":
-    image_name = '55075.jpg'
+    image_name = '135069.jpg'
     path = TRAIN_PATH + image_name
     img = Image.open(path)
-    img = resize_image(path, img.size[0] // 10, img.size[1] // 10)
+    img = resize_image(path, img.size[0] // 5, img.size[1] // 5)
     image_data = img.getdata()
-    clustering = NCut(image_data, 3, gamma=10, algorithm=NCut.RBF_KERNEL)
+    show_image_from_data(img.mode, img.size, image_data)
+    clustering = NCut(image_data, 2, gamma=1, algorithm=NCut.RBF_KERNEL)
     new_image_data = clustering.assign()
     show_image_from_data(img.mode, img.size, new_image_data)
