84d8444899631f9081b8f8cbc913a4391e21ff07
shakram02 <ahmedhamdyau@gmail.com>
2017-11-28 05:09:56 +0200

    some elegance, remove extension from image name

--- data_reader.py
+++ data_reader.py
@@ -1,12 +1,13 @@
-from glob import glob
-from PIL import Image
-from numpy import array
 from os import listdir
 from os.path import join, isfile
+
+from PIL import Image
+from numpy import array
 
 TRAIN_PATH = 'BSR/BSDS500/data/images/train/'
 TEST_PATH = 'BSR/BSDS500/data/images/test/'
 GROUND_TRUTH_TEST_PATH = "BSR/BSDS500/data/groundTruth/test/"
+TRUTH_SEG_COUNT = 4  # Number of different segmentations per image in GroundTruth
 
 
 def read_image(path):
@@ -16,9 +17,10 @@ def read_image(path):
 
 def read_ground_truth(folder_path):
     from scipy.io import loadmat
-    extension = ".mat"
     import numpy
-    import matplotlib.pyplot as plt
+    # import pylab as plt
+
+    extension = ".mat"
 
     file_names = [x for x in listdir(folder_path)]
     file_paths = [join(folder_path, x) for x in listdir(folder_path)]
@@ -27,15 +29,17 @@ def read_ground_truth(folder_path):
     segmented_data = {}
 
     for image_name, image_path in zip(file_names, image_paths):
-        segmented_data[image_name] = []
-
         mat = loadmat(image_path)
         im = mat["groundTruth"]
         images: numpy.ndarray = im
 
-        for i in range(4):
+        table_key = image_name.replace(extension, "")
+        segmented_data[table_key] = []
+
+        for i in range(TRUTH_SEG_COUNT):
+            # Couldn't make it pretty
             image = images[0][i][0][0][0]
-            segmented_data[image_name].append(image)
+            segmented_data[table_key].append(image)
 
             # Use for testing
             # plt.imshow(image)
