2c88af0b78c91daca4364f975e155e0bd8e807e8
shakram02 <ahmedhamdyau@gmail.com>
2017-11-29 09:16:52 +0200

    conditional entropy

--- evaluator.py
+++ evaluator.py
@@ -1,15 +1,29 @@
 import math
 
 
-def conditional_entropy(k, ni, nij):
+def conditional_entropy(result, truth):
+    # Input is a list, index:pixel , value: cluster
     entropy = 0
+    result_counts = count_clusters(result)
+    truth_counts = count_clusters(truth)
 
-    for idx in range(k):
-        percentage = nij[idx] / ni[idx]
+    for idx in truth_counts.keys():
+        nij = result_counts[idx]  # Correct elements
+        ni = truth_counts[idx]  # Deduced elements
+
+        percentage = nij / ni
         entropy += percentage * math.log(percentage, 2)
 
     return -1 * entropy
 
 
-def f_measure(, k, ni, nij):
-    pass
+def count_clusters(items):
+    counts = {}
+
+    for (i, cluster) in enumerate(items):
+        if cluster not in counts:
+            counts[cluster] = 1
+        else:
+            counts[cluster] += 1
+
+    return counts
