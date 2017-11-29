3a3e678658ec82040d6e779f566efff3306ae5b4
shakram02 <ahmedhamdyau@gmail.com>
2017-11-29 15:30:13 +0200

    fix conditional entropy, previously screwed up with notations

--- evaluator.py
+++ evaluator.py
@@ -4,7 +4,7 @@ import math
 def conditional_entropy(result, truth):
     # Input is a list, index:pixel , value: cluster
     entropy = 0
-    result_counts = count_clusters(result)
+    result_counts = count_correctly_clustered(result, truth)
     truth_counts = count_clusters(truth)
 
     for idx in truth_counts.keys():
@@ -17,7 +17,49 @@ def conditional_entropy(result, truth):
     return -1 * entropy
 
 
+def calculate_purity(result_cluster_stats, truth_cluster_stats):
+    purity = 0
+    # Sum of values in the truth is the total number of pixels
+    total_pixel_count = sum(truth_cluster_stats.values())
+
+    for value in result_cluster_stats.values():
+        purity += value / total_pixel_count
+
+    return purity
+
+
+def count_correctly_clustered(result, truth):
+    """
+    Computes the value n_ij for a given clustering and its ground truth
+    the n_ij value is the number of the truly identified pixels
+    :param result: Calculated clustering
+    :param truth: Ground truth clustering
+    :return: Nij for all clusters in a hashtable
+    """
+    # We shouldn't check for programmers mistakes, but meh,
+    # If this throws then the images might be differnet
+    assert len(result) == len(truth)
+
+    counts = {}
+
+    for x, y in zip(result, truth):
+        if x != y:
+            continue
+
+        # Create an entry in the table
+        if x not in counts:
+            counts[x] = 0
+
+        counts[x] += 1
+
+    return counts
+
+
 def count_clusters(items):
+    """
+    Counts the elements in all clusters in a given clustering
+    :param items: Result of the clustering process
+    """
     counts = {}
 
     for (i, cluster) in enumerate(items):
